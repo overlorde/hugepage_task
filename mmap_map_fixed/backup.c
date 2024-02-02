4426026691f9d5f328961e7a06ad1430ba5b80eb
Farhan Saif <farhansaif488@gmail.com>
2024-02-02 19:26:17 +0600

    final

--- mmap_map_fixed/backup.c
+++ mmap_map_fixed/backup.c
@@ -27,7 +27,7 @@ typedef struct{
 }hugepage_info;
 
 void hugepage_memory_free(hugepage_info *hugepage_info_ptr) {
-	for (int i = 0; i < 2/*NUM_HUGEPAGES*/; i++) {
+	for (int i = 0; i < 10/*NUM_HUGEPAGES*/; i++) {
 		if (hugepage_info_ptr[i].hugepage_addr != NULL) {
 			if(munmap(hugepage_info_ptr[i].hugepage_addr, HUGEPAGE_SIZE) == -1) {
 				perror("hugepage_memory_free: munmap failed");
@@ -37,141 +37,215 @@ void hugepage_memory_free(hugepage_info *hugepage_info_ptr) {
 	}
 }
 
-void create_hugepage_A(hugepage_info *a, int fd_1, int i)
-{
-
-    a->hugepage_addr = mmap(NULL,HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_POPULATE, fd_1, 0);
-    if (a->hugepage_addr == MAP_FAILED) {
-        perror("Error mapping memory");
-        close(fd_1);
-        exit(EXIT_FAILURE);
-    }
-    else{
-	printf("hugepage allocation success\n");
-	}
-}
-void create_hugepage_B(hugepage_info *a, int fd_2, int i)
-{
-
-    a->hugepage_addr = mmap(NULL,HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_POPULATE, fd_2, 0);
-    if (a->hugepage_addr == MAP_FAILED) {
-        perror("Error mapping memory");
-        close(fd_2);
-        exit(EXIT_FAILURE);
-    }
-    else{
-	printf("hugepage allocation success\n");
-	}
-}
 
 //5 bytes each
 void object_allocation_A(hugepage_info *a)
 {
-    for(int i=0; i<10; i++)
+    for(int i=0; i<65536; i++)
     {
 	if(i%2 == 0)
 	{
-	    memset(a->hugepage_addr + i*5, 'A', 5);
+	    memset(a->hugepage_addr + i*32, 'A', 32);
 	}
     }
 }
 
+void object_copy(void *a, void *b )
+{
+
+    for(int i=0; i<65536; i++)
+    {
+	if(i%2 == 0)
+	{
+	    memcpy(b + i*32, a + i*32, 32);
+	}
+    }
+
+    memset(a, 0, 2*1024*1024);
+}
+
+
 void object_allocation_B(hugepage_info *a)
 {
-    for(int i=0; i<10; i++)
+    for(int i=0; i<65536; i++)
     {
 	if(i%2 == 1)
 	{
-	    memset(a->hugepage_addr + i*5, 'B', 5);
+	    memset(a->hugepage_addr + i*32, 'B', 32);
 	}
     }
 }
 
 int main()
 {
+    int fds[10];
 
-    int fd_1 = open("/mnt/hugetlbfs/hugepage_1", O_CREAT | O_RDWR, 0755);
-    int fd_2 = open("/mnt/hugetlbfs/hugepage_2", O_CREAT | O_RDWR, 0755);
-    if (fd_1 < 0) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    if (fd_2 < 0) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
+    fds[0] = open("/mnt/hugetlbfs/hugepage_1", O_CREAT | O_RDWR, 0755);
+    fds[1] = open("/mnt/hugetlbfs/hugepage_2", O_CREAT | O_RDWR, 0755);
+    fds[2] = open("/mnt/hugetlbfs/hugepage_3", O_CREAT | O_RDWR, 0755);
+    fds[3] = open("/mnt/hugetlbfs/hugepage_4", O_CREAT | O_RDWR, 0755);
+    fds[4] = open("/mnt/hugetlbfs/hugepage_5", O_CREAT | O_RDWR, 0755);
+    fds[5] = open("/mnt/hugetlbfs/hugepage_6", O_CREAT | O_RDWR, 0755);
+    fds[6] = open("/mnt/hugetlbfs/hugepage_7", O_CREAT | O_RDWR, 0755);
+    fds[7] = open("/mnt/hugetlbfs/hugepage_8", O_CREAT | O_RDWR, 0755);
+    fds[8] = open("/mnt/hugetlbfs/hugepage_9", O_CREAT | O_RDWR, 0755);
+    fds[9] = open("/mnt/hugetlbfs/hugepage_10", O_CREAT | O_RDWR, 0755);
 
     // Seek to the end of the file to make it large enough
-    if (lseek(fd_1, (1 * HUGEPAGE_SIZE) - 1, SEEK_SET) == -1) {
-        perror("lseek");
-        exit(EXIT_FAILURE);
-    }
 
-    if (lseek(fd_2, (1 * HUGEPAGE_SIZE) - 1, SEEK_SET) == -1) {
-        perror("lseek");
-        exit(EXIT_FAILURE);
-    }
-
-    if (ftruncate(fd_1, 1 * HUGEPAGE_SIZE) == -1) {
+    /*if (ftruncate(fd_1, 5 * HUGEPAGE_SIZE) == -1) {
         perror("ftruncate");
         exit(EXIT_FAILURE);
     }
 
-    if (ftruncate(fd_2, 1 * HUGEPAGE_SIZE) == -1) {
+    if (ftruncate(fd_2, 5 * HUGEPAGE_SIZE) == -1) {
         perror("ftruncate");
         exit(EXIT_FAILURE);
-    }
+    }*/
 
 
-
-    hugepage_info hparray[2];
-    for(int i=0; i<2; i++)
+    hugepage_info hparray[10];
+    for(int i=0; i<10; i++)
     {
 	    hparray[i].hugepage_addr = NULL;
 	    if(i%2==0)
 	    {
-	    create_hugepage_A(&hparray[i], fd_1, i);
-	    object_allocation_A(&hparray[i]);
+	    hparray[i].hugepage_addr = mmap(NULL,HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_POPULATE, fds[i], 0);
+		object_allocation_A(&hparray[i]);
 	    }
 	    else{
-	    create_hugepage_B(&hparray[i], fd_2, i);
-	    object_allocation_B(&hparray[i]);
+	    hparray[i].hugepage_addr = mmap(NULL,HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_POPULATE, fds[i], 0);
+		object_allocation_B(&hparray[i]);
 	    }
     }
+// object transfer
+    printf("transferring operation started \n");
 
-    printf("Copying operation started \n");
-    memcpy(hparray[1].hugepage_addr, hparray[0].hugepage_addr, 5);
 
-    printf("Copying operation ended \n");
+    for(int i = 0; i < 10; i+=2)
+    {
+	object_copy(hparray[i].hugepage_addr, hparray[i+1].hugepage_addr);
+    }
 
+    printf("transferring operation ended \n");
+
+    // unmapping
+//1
     void *addr_1 = hparray[0].hugepage_addr;
     if (munmap(addr_1, HUGEPAGE_SIZE) == -1) {
         perror("munmap failed");
         return 1;
     }
+    close(fds[0]);
+    unlink("mnt/hugetlbfs/hugepage_1");
 
-  void *addr_2 = mmap(addr_1, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fd_2, 0);
+  void *addr_2 = mmap(addr_1, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fds[1], 0);
     if (addr_2 == MAP_FAILED) {
         perror("Remap failed");
         return 1;
     }
 
-    printf(" huge page mapped at address_1: %p\n", addr_1);
-    printf(" huge page mapped at address_2: %p\n", addr_2);
-    printf(" huge page mapped at address for 2nd hugepage: %p\n", hparray[1].hugepage_addr);
-
-
-
-    /*
-    for (int i = 0; i < 10; ++i) {
-	printf("%d Object A - %c\n",i, object_1[i]);
-	printf("%d Object B - %c\n",i, object_2[i]);
+    printf(" huge page 1 mapped at address_2: %p\n", addr_2);
+    printf(" huge page 2 mapped at address for 2nd hugepage: %p\n", hparray[1].hugepage_addr);
+ //2
+    void *addr_3 = hparray[2].hugepage_addr;
+    if (munmap(addr_1, HUGEPAGE_SIZE) == -1) {
+        perror("munmap failed");
+        return 1;
     }
-    */
+
+    close(fds[2]);
+    unlink("mnt/hugetlbfs/hugepage_3");
+
+    void *addr_4 = mmap(addr_3, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fds[3], 0);
+    if (addr_2 == MAP_FAILED) {
+        perror("Remap failed");
+        return 1;
+    }
+
+//3
+    void *addr_5 = hparray[4].hugepage_addr;
+    if (munmap(addr_1, HUGEPAGE_SIZE) == -1) {
+        perror("munmap failed");
+        return 1;
+    }
+
+    close(fds[4]);
+    unlink("mnt/hugetlbfs/hugepage_5");
+
+
+    void *addr_6 = mmap(addr_5, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fds[5], 0);
+    if (addr_2 == MAP_FAILED) {
+        perror("Remap failed");
+        return 1;
+    }
+
+//4
+
+    void *addr_7 = hparray[6].hugepage_addr;
+    if (munmap(addr_1, HUGEPAGE_SIZE) == -1) {
+        perror("munmap failed");
+        return 1;
+    }
+
+    close(fds[6]);
+    unlink("mnt/hugetlbfs/hugepage_7");
+
+    void *addr_8 = mmap(addr_7, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fds[7], 0);
+    if (addr_2 == MAP_FAILED) {
+        perror("Remap failed");
+        return 1;
+    }
+
+//5
+
+    void *addr_9 = hparray[8].hugepage_addr;
+    if (munmap(addr_1, HUGEPAGE_SIZE) == -1) {
+        perror("munmap failed");
+        return 1;
+    }
+
+    close(fds[8]);
+    unlink("mnt/hugetlbfs/hugepage_9");
+
+    void *addr_10 = mmap(addr_9, HUGEPAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_HUGETLB | MAP_FIXED, fds[9], 0);
+    if (addr_2 == MAP_FAILED) {
+        perror("Remap failed");
+        return 1;
+    }
+
+
+
+    printf("hugepage_2 page object \n");
+    char *object_1;
+    char *object_2;
+    //hparray[0].hugepage_addr = addr_2;
+    object_1 = (char*)hparray[2].hugepage_addr;
+    object_2 = (char*)hparray[3].hugepage_addr;
+
+    printf(" hugepage 1 starting vaddr: %p\n", hparray[2].hugepage_addr);
+
+    printf(" hugepage 2 starting vaddr: %p\n", hparray[3].hugepage_addr);
+
+
+    for (int i = 0; i < 64; ++i) {
+	printf("%d Object A - %c\n",i, object_1[i]);
+    }
 
     hugepage_memory_free(hparray);
-    close(fd_1);
-    close(fd_2);
+    for(int i=1; i<=10; i++)
+	{
+	    if(i%2==1)
+	    {
+		close(fds[i]);
+	    }
+	}
+
+
+    unlink("mnt/hugetlbfs/hugepage_2");
+    unlink("mnt/hugetlbfs/hugepage_4");
+    unlink("mnt/hugetlbfs/hugepage_6");
+    unlink("mnt/hugetlbfs/hugepage_8");
+    unlink("mnt/hugetlbfs/hugepage_10");
 }
 
